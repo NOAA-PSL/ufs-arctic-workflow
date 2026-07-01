cd148fc843c8c6e960419c85baf4b1d68db7efa4
Kristin Barton <Kristin.Barton@noaa.gov>
2026-07-01 16:47:06 -0600

    Fixes to coupling (#16)
    * Turn on fractional grid in input.nml templates
    
    * Update ufs.arctic regridding polemethod to none in CMEPS
    
    * Update sfc and grid file pointers to correct halo sizes

--- workflow/submit_workflow.sh
+++ workflow/submit_workflow.sh
@@ -193,6 +193,8 @@ setup() {
     cp -L "${FIX_DIR}/input_grid_files/ice/"*.nc "${MODEL_DIR}/INPUT/"
     cp -P "${FIX_DIR}/datasets/run_dir/"* "${MODEL_DIR}/"
 
+    ln -sf "${ATM_RES}_grid.tile7.halo3.nc" "${MODEL_DIR}/INPUT/${ATM_RES}_grid.tile7.nc"
+
     cp -P "${UFS_DIR}/modulefiles/ufs_${SYSTEM}.${COMPILER}.lua" "${MODEL_DIR}/modulefiles/modules.fv3.lua"
     cp -P "${UFS_DIR}/modulefiles/ufs_common.lua" "${MODEL_DIR}/modulefiles/"
     ln -sf "${UFS_DIR}/build/ufs_model" "${MODEL_DIR}/fv3.exe"
@@ -215,16 +217,25 @@ setup() {
     cp -P ${CONFIG_DIR}/templates/${ATM_RES}/input.nml ${MODEL_DIR}/.
     cp -P ${CONFIG_DIR}/templates/${ATM_RES}/MOM_input ${MODEL_DIR}/.
 
-    ln -sf ${ATM_RES}.facsf.tile7.halo4.nc ${MODEL_DIR}/${ATM_RES}.facsf.tile1.nc                
-    ln -sf ${ATM_RES}.slope_type.tile7.halo4.nc ${MODEL_DIR}/${ATM_RES}.slope_type.tile1.nc       
-    ln -sf ${ATM_RES}.soil_color.tile7.halo4.nc ${MODEL_DIR}/${ATM_RES}.soil_color.tile1.nc  
-    ln -sf ${ATM_RES}.substrate_temperature.tile7.halo4.nc ${MODEL_DIR}/${ATM_RES}.substrate_temperature.tile1.nc  
-    ln -sf ${ATM_RES}.vegetation_type.tile7.halo4.nc ${MODEL_DIR}/${ATM_RES}.vegetation_type.tile1.nc
-    ln -sf ${ATM_RES}.maximum_snow_albedo.tile7.halo4.nc ${MODEL_DIR}/${ATM_RES}.maximum_snow_albedo.tile1.nc  
-    ln -sf ${ATM_RES}.snowfree_albedo.tile7.halo4.nc ${MODEL_DIR}/${ATM_RES}.snowfree_albedo.tile1.nc  
-    ln -sf ${ATM_RES}.soil_type.tile7.halo4.nc ${MODEL_DIR}/${ATM_RES}.soil_type.tile1.nc   
-    ln -sf ${ATM_RES}.vegetation_greenness.tile7.halo4.nc ${MODEL_DIR}/${ATM_RES}.vegetation_greenness.tile1.nc
-    
+    ln -sf ${ATM_RES}.facsf.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.facsf.tile1.nc                
+    ln -sf ${ATM_RES}.facsf.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.facsf.tile7.nc                
+    ln -sf ${ATM_RES}.slope_type.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.slope_type.tile1.nc       
+    ln -sf ${ATM_RES}.slope_type.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.slope_type.tile7.nc       
+    ln -sf ${ATM_RES}.soil_color.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.soil_color.tile1.nc  
+    ln -sf ${ATM_RES}.soil_color.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.soil_color.tile7.nc  
+    ln -sf ${ATM_RES}.substrate_temperature.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.substrate_temperature.tile1.nc  
+    ln -sf ${ATM_RES}.substrate_temperature.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.substrate_temperature.tile7.nc  
+    ln -sf ${ATM_RES}.vegetation_type.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.vegetation_type.tile1.nc
+    ln -sf ${ATM_RES}.vegetation_type.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.vegetation_type.tile7.nc
+    ln -sf ${ATM_RES}.maximum_snow_albedo.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.maximum_snow_albedo.tile1.nc  
+    ln -sf ${ATM_RES}.maximum_snow_albedo.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.maximum_snow_albedo.tile7.nc  
+    ln -sf ${ATM_RES}.snowfree_albedo.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.snowfree_albedo.tile1.nc  
+    ln -sf ${ATM_RES}.snowfree_albedo.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.snowfree_albedo.tile7.nc  
+    ln -sf ${ATM_RES}.soil_type.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.soil_type.tile1.nc   
+    ln -sf ${ATM_RES}.soil_type.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.soil_type.tile7.nc   
+    ln -sf ${ATM_RES}.vegetation_greenness.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.vegetation_greenness.tile1.nc
+    ln -sf ${ATM_RES}.vegetation_greenness.tile7.halo0.nc ${MODEL_DIR}/${ATM_RES}.vegetation_greenness.tile7.nc
+
     render_template "${CONFIG_DIR}/templates/${ATM_RES}/ice_in" "${MODEL_DIR}/ice_in"
     render_template "${CONFIG_DIR}/templates/${ATM_RES}/diag_table" "${MODEL_DIR}/diag_table"
     render_template "${CONFIG_DIR}/templates/${ATM_RES}/model_configure" "${MODEL_DIR}/model_configure"
