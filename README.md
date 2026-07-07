e2b7f71166dbfa511d73c995f8db6e004dbb0a26
Kristin Barton <kristin.barton@noaa.gov>
2026-07-07 23:13:34 +0000

    Add atmosphere LBC parallelism and improve resource allocation

--- README.md
+++ README.md
@@ -9,14 +9,14 @@ Ursa
     `git submodule update --init --recursive`
 2. Compile the UFS model:
     `sbatch --account <job-account> compile_ufs.sh`
-3. Open the wrapper script `run_workflow.sh` to configure your experiment(s).
+3. Open the wrapper script `submit_workflow.sh` to configure your experiment(s).
    Adjust the slurm account, run length, resolution, dates, etc.
 4. Execute the workflow script: 
-    `./run_workflow.sh`
+    `./submit_workflow.sh`
 Run workflow provides a template for setting up and running multiple experiments at once.
 
 (5. Alternatively, you can call the batch submission directly from the command line:
-    `sbatch <sbatch_options> ./workflow/submit_workflow.sh --date <YYYYMMDD> --hours <NHRS> --res <CRES> --run-dir </PATH/TO/OUTPUT/DIR> --job-name <JOB_NAME>`)
+    `sbatch <sbatch_options> ./workflow/run_workflow.sh --date <YYYYMMDD> --hours <NHRS> --res <CRES> --run-dir </PATH/TO/OUTPUT/DIR> --job-name <JOB_NAME>`)
 
 
 (*Optional*) If you have an existing, pre-compiled UFS directory you would like to work from,
--- submit_workflow.sh
+++ submit_workflow.sh
@@ -5,39 +5,40 @@
 # ================================= #
 
 SACCT="ufs-artic"       # Account for job submission
-HOURS=3                 # Model forecast length (Max: 240 Hours)
+HOURS=240               # Model forecast length (Max: 240 Hours)
 RES=(                   # Model resolution (C918 ~11km; C185 ~50km)
-#    "C918"
-    "C185"
+    "C918"
+#    "C185"
 )
 DATES=(                 # Format: YYYYMMDD
-    "20191028"          # Options: 20191028 | 20200227 | 20200702 | 20200709 | 20200827
+#    "20191028"          # Options: 20191028 | 20200227 | 20200702 | 20200709 | 20200827
 #    "20200227"
-#    "20200702"
+    "20200702"
 #    "20200709"
 #    "20200827"
 )
 # Optional: Specify pre-compiled directory. Leave blank to run from current directory.
-#UFS_DIR="/scratch4/BMC/ufs-artic/Kristin.Barton/repos/kristinbarton/ufs-arctic-workflow/build/C5203a784/ufs-weather-model/"              
-UFS_DIR=""
+UFS_DIR="/scratch4/BMC/ufs-artic/Kristin.Barton/repos/kristinbarton/ufs-arctic-workflow/build/C8db7efa4/ufs-weather-model/"       
+#UFS_DIR=""
 
-BASE_RUN_DIR="/scratch4/BMC/${SACCT}/${USER}/stmp" # Output will go in ${BASE_RUN_DIR}/${JOB_NAME}
+BASE_RUN_DIR="/scratch4/BMC/${SACCT}/${USER}/stmp/resource_tests" # Output will go in ${BASE_RUN_DIR}/${JOB_NAME}
 
 # ================================= #
 # Other SLURM Options               #
 # ================================= #
 
-QOS="batch"             # Specify QOS
+QOS="debug"             # Specify QOS
 TIME="00:30:00"         # C918 may take longer than 60m. C185 should be less than 30m
-NODES=2                 # Specify nodes
-NTASKS=4                # Specify tasks
+NODES=1                 # Specify nodes
+NTASKS=30              # Specify tasks - Must be multiples of 6
+CPUS=2                  # CPUS per task - Must be >= 2
 
 # ================================= #
 # Execution Loop                    #
 # ================================= #
 
 echo "Starting batch submission..."
-SCRIPT="./workflow/submit_workflow.sh"
+SCRIPT="./workflow/run_workflow.sh"
 
 for d in "${DATES[@]}"; do
 for r in "${RES[@]}"; do
@@ -53,6 +54,7 @@ for r in "${RES[@]}"; do
         "--time=$TIME"
         "--nodes=$NODES"
         "--ntasks=$NTASKS"
+        "--cpus-per-task=$CPUS"
         "--job-name=Prep_${JOB_NAME}"
         "$SCRIPT"
         "--date" "$d"
@@ -64,6 +66,11 @@ for r in "${RES[@]}"; do
     if [[ -n "UFS_DIR" ]]; then
         CMD+=("--ufs-dir" "$UFS_DIR")
     fi
+
+    # Uncomment one of these if you want to run only a single prep step
+    #CMD+=("--step" "prep_atm")
+    #CMD+=("--step" "prep_ocn")
+    #CMD+=("--step" "prep_ice")
 
     # Uncomment this if you want to prep the model run WITHOUT submitting the final job
     #CMD+=("--norun")
